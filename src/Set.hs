60c2025ff651824152f605e68affb7e8f8572bea
Alex Nelson <pqnelson@gmail.com>
2015-02-12 20:42:38 -0800

    Need to remove elts from set

--- src/Set.hs
+++ src/Set.hs
@@ -20,6 +20,8 @@ module Set
        , isSubset
        , isProperSubset
        , powerSet
+       , (\\)
+       , remove
        ) where
 import qualified Data.List as List
 
@@ -42,23 +44,27 @@ union' s1@(h1:t1) s2@(h2:t2) = case compare h1 h2 of
                                 LT -> h1 : union' t1 s2
                                 GT -> h2 : union' s1 t2
 
+-- | The union of two sets.
 union :: Ord a => [a] -> [a] -> [a]
 union l1 l2 = union' (setify l1) (setify l2)
 
 intersect' :: Ord a => [a] -> [a] -> [a]
-intersect' [] l2 = []
-intersect' l1 [] = []
+intersect' [] _ = []
+intersect' _ [] = []
 intersect' l1@(h1:t1) l2@(h2:t2) = case compare h1 h2 of
                                     EQ -> h1 : intersect' t1 t2
                                     LT -> intersect' t1 l2
                                     GT -> intersect' l1 t2
 
+-- | The intersection of two sets
 intersect :: Ord a => [a] -> [a] -> [a]
 intersect l1 l2 = intersect' (setify l1) (setify l2)
 
+-- | Inserting an element into a set
 insert :: Ord a => a -> [a] -> [a]
 insert x = union [x]
 
+-- | Checks if first argument is a subset (proper or not) of the second argument
 isSubset :: Ord a => [a] -> [a] -> Bool
 isSubset [] _ = True
 isSubset _ [] = False
@@ -66,14 +72,30 @@ isSubset l1@(h1:t1) (h2:t2) | h1 == h2 = isSubset t1 t2
                             | h1 < h2 = False
                             | otherwise = isSubset l1 t2
 
+-- | Checks if the first argument is a proper subset of the second
 isProperSubset :: Ord a => [a] -> [a] -> Bool
 isProperSubset _ [] = False
 isProperSubset [] _ = True
 isProperSubset s1 s2 = (s1 /= s2) && isSubset s1 s2
 
+-- | Returns the set of all subsets of the argument
 powerSet :: [a] -> [[a]]
 powerSet [] = [[]]
 powerSet (a:t) = let acc = powerSet t
                  in map (a:) acc ++ acc
 
+subtractSet :: Ord a => [a] -> [a] -> [a]
+subtractSet [] _ = []
+subtractSet s1 [] = s1
+subtractSet s1@(h1:t1) s2@(h2:t2) = case compare h1 h2 of
+                                     EQ -> subtractSet t1 t2
+                                     LT -> h1 : subtractSet t1 s2
+                                     GT -> subtractSet s1 t2
 
+-- | The set difference of the first list minus the second list
+(\\) :: Ord a => [a] -> [a] -> [a]
+s1 \\ s2 = subtractSet (setify s1) (setify s2)
+
+-- | Removes a single element from a set
+remove :: Ord a => [a] -> a -> [a]
+remove s1 elt = s1 \\ [elt]
